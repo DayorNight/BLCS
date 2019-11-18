91e81e6939ddbe135ab229a2fc51f13a8df40977
BLCS <2302940971@qq.com>
2019-11-18 17:18:34 +0800

    1.DataBinding例子

--- app/src/main/java/blcs/lwb/utils/fragment/otherFragment/Jetpack/DataBindingFragment.java
+++ app/src/main/java/blcs/lwb/utils/fragment/otherFragment/Jetpack/DataBindingFragment.java
@@ -1,12 +1,27 @@
 package blcs.lwb.utils.fragment.otherFragment.Jetpack;
 
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import blcs.lwb.lwbtool.base.BasePresenter;
 import blcs.lwb.utils.R;
+import blcs.lwb.utils.adapter.ListAdapter;
+import blcs.lwb.utils.databinding.FragmentDatabindingBinding;
 import blcs.lwb.utils.fragment.BaseFragment;
 
-public class DataBindingFragment extends BaseFragment {
+public class DataBindingFragment extends BaseFragment implements View.OnClickListener {
+
+    private FragmentDatabindingBinding bind;
+    private List<String> datas = new ArrayList<>();
+    private ListAdapter listAdapter;
 
     @Override
     protected int bindLayout() {
@@ -15,9 +30,20 @@ public class DataBindingFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        bind = DataBindingUtil.bind(mView);
+        bind.setLifecycleOwner(this);
+        bind.setClick(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false);
+        listAdapter = new ListAdapter();
+        bind.setManager(linearLayoutManager);
+        bind.setListAdapter(listAdapter);
 
     }
-
+    //自定义属性 设置适配器
+    @BindingAdapter("adapter")
+    public static void setAdapter(RecyclerView view, RecyclerView.Adapter adapter) {
+        view.setAdapter(adapter);
+    }
     @Override
     public void setMiddleTitle(Toolbar title) {
 
@@ -31,5 +57,11 @@ public class DataBindingFragment extends BaseFragment {
     @Override
     public void popBackListener(int returnCode, Bundle bundle) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        listAdapter.addData(bind.getContent());
+        bind.setContent("");
     }
 }
